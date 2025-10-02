873f109599a8a07f40c47ba5c970c2bf192c310f
Takshil Kunadia <takshilkunadia@gmail.com>
2025-10-02 15:26:42 +0530

    init

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ Plugin Name: Notion2WP
+ Description: Publish content directly from Notion to WordPress.
+ Version: 0.1.0
+ Author: Takshil Kunadia
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
